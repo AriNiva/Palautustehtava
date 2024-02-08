052d8aa7c81c3543acb4b8fcf7cd4e5214b2f4b4
AriNiva <arin.postiloota@outlook.com>
2024-02-08 14:01:52 +0200

    Toiminnallisuus kai ok. Ulkoasu ei

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const henkilot = require("./henkilot.json")
+
+app.get("/api/henkilot", (req, res) => {
+    res.json(henkilot)
+});
+
 const polku = path.join(__dirname, "./public");
 app.use(express.static(polku));
 app.listen(3000, () => {
--- public/script.js
+++ public/script.js
@@ -14,3 +14,21 @@ function naytaYhteystiedot() {
                    <p>Tässä on yhteystiedot</p>`
     document.getElementById("main_alue").innerHTML = yhteystiedot;
 }
+
+async function fetchData() {
+    var x = `<table><thead><th>ID</th><th>Nimi</th><th>Osoite</th><th>Puhelin</th></thead><tbody>`
+    try {
+        const response = await fetch("http://localhost:3000/api/henkilot")
+        const henkilodata = await response.json()
+        await henkilodata.map(h => {
+            x += `<tr><td>${h.id}</td><td>${h.name}</td><td>${h.address}</td><td>${h.phone}</td></tr>`
+        })
+        x += `</tbody></table>`
+        document.getElementById("main_alue").innerHTML = x
+    }
+    catch (error) {
+        console.error("Error fetching data:", error)
+    }
+}
+                 
+                     
